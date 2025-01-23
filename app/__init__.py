64421384003834e9d869c0e961eb4ddab9a5ff81
bonny39 <bonny3780cpc@gmail.com>
2025-01-23 13:52:57 +0800

    add tukey page and model

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,12 @@ def create_app():
 
     # 載入/註冊各個Blueprint
     from app.routes.main_routes import main_bp
+    from app.routes.tukey_routes import tukey_bp
     from app.routes.xgb_routes import xgb_bp
     from app.routes.prophet_routes import prophet_bp
 
     app.register_blueprint(main_bp)
+    app.register_blueprint(tukey_bp)
     app.register_blueprint(xgb_bp)
     app.register_blueprint(prophet_bp)
 
