f38ab1f023f69c3b08c29473330d3e5bb9d23a2b
cpchsh <cpchsh319@gmail.com>
2025-02-03 16:31:51 +0800

    調整blueprint路徑問題和圖表顯示

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify
-import joblib
+
 import pandas as pd
 import os
 import matplotlib
@@ -8,13 +8,13 @@ matplotlib.use('Agg')
 app = Flask(__name__)
 app.secret_key = "anyrandomsecret"  # 若要使用flash或session需設
 
-#======================#
-#   1) 載入 XGB 模型   #
-#======================#
-# 假設你已在其他地方用 joblib.dump(xgb_model, "xgb_model.pkl")
-# 並把檔案放在專案根目錄
-xgb_model_path = "./xgb_model_new.pkl"
-xgb_model = joblib.load(xgb_model_path)
+# #======================#
+# #   1) 載入 XGB 模型   #
+# #======================#
+# # 假設你已在其他地方用 joblib.dump(xgb_model, "xgb_model.pkl")
+# # 並把檔案放在專案根目錄
+# xgb_model_path = "./xgb_model_new.pkl"
+# xgb_model = joblib.load(xgb_model_path)
 
 
 #======================#
@@ -85,45 +85,47 @@ def predict_next_day_xgb(xgb_model):
 #======================#
 #   3) 首頁(顯示預測)   #
 #======================#
-@app.route("/")
-def index():
-    # 1) 用 XGB 預測下一天
-    next_day_pred = predict_next_day_xgb(xgb_model)
-    next_day_pred_str = f"{next_day_pred:.2f}"  # 格式化
+# @app.route("/")
+# def index():
+#     # 1) 用 XGB 預測下一天
+#     next_day_pred = predict_next_day_xgb(xgb_model)
+#     next_day_pred_str = f"{next_day_pred:.2f}"  # 格式化
     
-    # 1) 讀取預測結果 CSV (latest_forecast.csv)
-    if os.path.exists("./latest_forecast.csv"):
-        forecast = pd.read_csv("latest_forecast.csv")
-        # 取最後 7 天的預測
-        future_part = forecast.tail(7)[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
-        # 重新命名欄位名稱
-        future_part = future_part.rename(columns={
-            'ds': '日期',
-            'yhat': '預測值',
-            'yhat_lower': '預測信賴區間下界',
-            'yhat_upper': '預測信賴區間上界'
-        })
-        #print("future_part",future_part)
-        value = future_part.iloc[0,1]
-        f_value = f"{value:.2f}"
-        f_day = future_part.iloc[0,0]
-        future_table_html = future_part.to_html(index=False)
-    else:
-        f_day = "N/A"
-        f_value="N/A"
-        future_table_html = "<p>No forecast data found. Please check if the offline script has run.</p>"
+#     # 1) 讀取預測結果 CSV (latest_forecast.csv)
+#     if os.path.exists("./latest_forecast.csv"):
+#         forecast = pd.read_csv("latest_forecast.csv",encoding="utf-8-sig")
+#         forecast.columns = forecast.columns.str.strip()  # 去除欄位名稱前後的空白
+#         # 取最後 7 天的預測
+#         future_part = forecast.tail(7)[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+#         # 重新命名欄位名稱
+#         future_part = future_part.rename(columns={
+#             'ds': '日期',
+#             'yhat': '預測值',
+#             'yhat_lower': '預測信賴區間下界',
+#             'yhat_upper': '預測信賴區間上界'
+#         })
+#         print("Renamed columns:", future_part.columns.tolist(), flush=True)
+#         print("future_part",future_part)
+#         value = future_part.iloc[0,1]
+#         f_value = f"{value:.2f}"
+#         f_day = future_part.iloc[0,0]
+#         future_table_html = future_part.to_html(classes="table custom-table text-center", index=False)
+#     else:
+#         f_day = "N/A"
+#         f_value="N/A"
+#         future_table_html = "<p>No forecast data found. Please check if the offline script has run.</p>"
     
-    # 2)圖檔檢查
-    plot_full_url = "static/plot_full.png" if os.path.exists("./static/plot_full.png") else ""
-    plot_recent_url = "static/plot_recent_future.png" if os.path.exists("./static/plot_recent_future.png") else ""
+#     # 2)圖檔檢查
+#     plot_full_url = "static/plot_full.png" if os.path.exists("./static/plot_full.png") else ""
+#     plot_recent_url = "static/plot_recent_future.png" if os.path.exists("./static/plot_recent_future.png") else ""
     
-    return render_template("index.html",
-                           next_day=f_day,
-                           xgb_pred=next_day_pred_str,
-                           prophet_fvalue=f_value,
-                           future_table_html=future_table_html,
-                           plot_full_url=plot_full_url,
-                           plot_recent_url=plot_recent_url)
+#     return render_template("index.html",
+#                            next_day=f_day,
+#                            xgb_pred=next_day_pred_str,
+#                            prophet_fvalue=f_value,
+#                            future_table_html=future_table_html,
+#                            plot_full_url=plot_full_url,
+#                            plot_recent_url=plot_recent_url)
 #================================#
 #   4) append + 重新訓練Prophet  #
 #================================#
@@ -234,6 +236,6 @@ def xgb_predict_custom():
                            s=singapore,sh=shanghai,zh=zhoushan)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)  # 除錯用
-    #app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(debug=True)  # 除錯用
+#     #app.run(host="0.0.0.0", port=5000)
