6f022b1b8c460ed884544ae4cc3ce7d68b012e48
cpchsh <cpchsh319@gmail.com>
2025-05-20 13:26:29 +0800

    自動化測試6 => 解決讀取不到資料庫讀取的參數問題2，且normalize line endings to LF

---
+++ start-cron.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+#1. 用envsubst替換模板中的 ${VAR}
+envsubst < /etc/cron.d/app-cron.tmpl > /etc/cron.d/app-cron
+
+#2. 修正權限並安裝到root的crontab
+chmod 0644 /etc/cron.d/app-cron
+crontab /etc/cron.d/app-cron
+
+echo "[INFO] Installed crontab ==="
+cat /etc/cron.d/app-cron
+echo "============================"
+
+#3. 前景模式跑cron
+exec cron -f
